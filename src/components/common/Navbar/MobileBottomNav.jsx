3241875989ac9a4fcadf0bc714b45dbba8f32c17
Moorthy <moorthy.chandiran21@gmail.com>
2025-03-24 10:42:08 +0530

    chnages done

--- src/components/common/Navbar/MobileBottomNav.jsx
+++ src/components/common/Navbar/MobileBottomNav.jsx
@@ -1,30 +1,45 @@
 import { FaHome, FaUser } from "react-icons/fa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IconCalendarWeek, IconMessage } from "@tabler/icons-react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const MobileBottomNav = () => {
-  const [active, setActive] = useState("home");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [active, setActive] = useState(location.pathname);
 
-  const getIconColor = (name) =>
-    active === name ? "text-[#8B78FF]" : "text-gray-500";
+  useEffect(() => {
+    setActive(location.pathname);
+  }, [location.pathname]);
+
+  const getIconColor = (path) =>
+    active === path ? "text-[#8B78FF]" : "text-gray-500";
 
   return (
     <div className="fixed bottom-0 left-0 w-full bg-white pt-3 flex justify-around items-center gap-1 rounded-xl">
       {[
-        { name: "home", icon: <FaHome size={22} /> },
-        { name: "calendar", icon: <IconCalendarWeek size={22} /> },
-        { name: "messages", icon: <IconMessage size={22} /> },
-        { name: "profile", icon: <FaUser size={22} /> },
-      ].map(({ name, icon }) => (
+        { name: "home", icon: <FaHome size={22} />, path: "/home" },
+        {
+          name: "calendar",
+          icon: <IconCalendarWeek size={22} />,
+          path: "/client-list",
+        },
+        {
+          name: "messages",
+          icon: <IconMessage size={22} />,
+          path: "/renewal-list",
+        },
+        { name: "profile", icon: <FaUser size={22} />, path: "/home" },
+      ].map(({ name, icon, path }) => (
         <button
           key={name}
-          onClick={() => setActive(name)}
+          onClick={() => navigate(path)}
           className={`flex flex-col items-center w-[80px] transition-all ${getIconColor(
-            name
+            path
           )}`}
         >
           {icon}
-          {active === name && (
+          {active === path && (
             <div className="w-[71px] h-[21px] mt-1 relative flex justify-center">
               <svg
                 viewBox="0 0 71 21"
